4fac8a9d426190a975f5493971eda0c701d37450
Baga6312 <magroubertn@gmail.com>
2024-06-14 19:41:06 +0100

    added some keybinding

---
+++ bspwm/nvim/init.lua
@@ -0,0 +1,23 @@
+require "user.options"
+require "user.keymaps" 
+require "user.plugins"
+require "user.colorscheme" 
+require "user.cmp" 
+require "user.lsp_config" 
+require "user.telescope"
+require "user.treesitter"
+require "user.autopairs"
+require "user.nvimtree"
+require "user.toggleterm" 
+
+
+vim.opt.termguicolors = true 
+
+require("nvim-tree").setup{}
+require("bufferline").setup{}
+require('hardline').setup {}
+
+
+vim.cmd(":resiz +2<CR>")
+vim.cmd(":NvimTreeToggle .")
+vim.cmd(":ToggleTerm . ")
