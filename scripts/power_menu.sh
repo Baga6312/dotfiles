aef0eb15e4288abfb462d861d934c0a02897c507
Baga6312 <magroubertn@gmail.com>
2024-06-21 02:21:57 +0100

    changes

--- scripts/power_menu.sh
+++ scripts/power_menu.sh
@@ -2,13 +2,13 @@
 
 # Power menu script using tofi
 
-CHOSEN=$(printf " Lock\nSuspend\nRestart\nShutdown\nLog Out" | rofi -dmenu -i -theme-str '@import "/home/dt/code/dotfiles-rice/bspwm/rofi/power.rasi" ') 
+CHOSEN=$(printf " \n\U0020\U23FB\n \U1F5D8\n\U0020\U23FB\n\U1F4E4" | rofi -dmenu -i -theme-str '@import "/home/dt/code/dotfiles-rice/bspwm/rofi/power.rasi" ') 
 
 case "$CHOSEN" in
 	"Lock") lockscreen ;;
-	"Suspend") systemctl suspend-then-hibernate ;;
+	"Suspend") systemctl suspend;;
 	"Reboot") reboot ;;
-	"Shutdown") poweroff ;;
-	"Log Out") hyprctl dispatch exit ;;
+	"Shutdown") shutdown now ;;
+	"Log Out") pkill -u $USER  exit ;;
 	*) exit 1 ;;
 esac 
