aae00f238eff2adea155781af4099e5a4c44994f
Baga6312 <magroubertn@gmail.com>
2024-06-22 02:12:49 +0100

    changes

--- scripts/power_menu.sh
+++ scripts/power_menu.sh
@@ -15,10 +15,10 @@ cancel="\Uf0156"
 CHOSEN=$(printf "$lockscreen\n$logoutt\n$suspendd\n$hibernate\n$reboot\n$shutdown "  | rofi -dmenu -i -theme-str '@import "/home/dt/code/dotfiles-rice/bspwm/rofi/power.rasi" ') 
 
 case "$CHOSEN" in
-	"Lock") lockscreen ;;
-	"Suspend") systemctl suspend;;
-	"Reboot") reboot ;;
-	"Shutdown") shutdown now ;;
-	"Log Out") pkill -u $USER  exit ;;
+	$lockscreen ) lockscreen ;;
+	$suspendd ) systemctl suspend;;
+	$reboot ) reboot ;;
+	$shutdown ) shutdown now ;;
+	$logout ) pkill -u $USER  exit ;;
 	*) exit 1 ;;
 esac 
