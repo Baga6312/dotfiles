990762274133576cc188efe20f09be7126c70ac0
Baga6312 <magroubertn@gmail.com>
2024-08-25 00:17:36 +0100

    changes

--- config/scripts/wifi-selector.sh
+++ config/scripts/wifi-selector.sh
@@ -12,7 +12,7 @@ elif [[ "$connected" =~ "disabled" ]]; then
 fi
 
 # Use rofi to select wifi network
-chosen_network=$(echo -e "$toggle\n$wifi_list" | uniq -u | rofi -dmenu -i -selected-row 1 -p "Wi-Fi SSID: " -theme-str '@import "/home/dt/code/dotfiles-main/bspwm/rofi/menu.rasi"')
+chosen_network=$(echo -e "$toggle\n$wifi_list" | uniq -u | rofi -dmenu -i -selected-row 1 -p "Wi-Fi SSID: " -theme-str '@import "/home/dt/code/dotfiles-main/config/bspwm/rofi/menu.rasi"')
 # Get name of connection
 read -r chosen_id <<< "${chosen_network:3}"
 
@@ -31,7 +31,7 @@ else
 		nmcli connection up id "$chosen_id" | grep "successfully" && notify-send "Connection Established" "$success_message"
 	else
 		if [[ "$chosen_network" =~ "" ]]; then
-			wifi_password=$(rofi -dmenu -p "Password: " )
+			wifi_password=$(rofi -dmenu -p "Password: " -theme-str '@import "/home/dt/code/dotfiles-main/config/bspwm/rofi/menu.rasi"')
 		fi
 		nmcli device wifi connect "$chosen_id" password "$wifi_password" | grep "successfully" && notify-send "Connection Established" "$success_message"
     fi
